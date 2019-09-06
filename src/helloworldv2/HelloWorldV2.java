44715fb22087249c8210a128560b6fd011a2f7a8
785264 <785264@ELF8IT221539.ACDM.DS.SAIT.CA>
2019-09-06 14:45:37 -0600

    Created 2 variables Made the console display the sum of num1 + num2

--- src/helloworldv2/HelloWorldV2.java
+++ src/helloworldv2/HelloWorldV2.java
@@ -17,6 +17,9 @@ public class HelloWorldV2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!!!");
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1 + num2);
     }
     
 }
